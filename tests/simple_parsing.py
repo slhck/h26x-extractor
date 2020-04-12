26c7d3e3dca632dd9b28a1d232a37345ea8a015a
Werner Robitza <werner.robitza@gmail.com>
2020-04-12 20:07:54 +0200

    formatting

--- tests/simple_parsing.py
+++ tests/simple_parsing.py
@@ -14,14 +14,12 @@ from h26x_extractor import h26x_parser
 class ParsingTest(unittest.TestCase):
     def testAUDParser(self):
         """Simple AUD parsing."""
-        blob = '0x000000010910'
+        blob = "0x000000010910"
         # create parser
         ex = h26x_parser.H26xParser(None, verbose=True, use_bitstream=blob)
         # make sure decode is happy
         ex.parse()
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
-
