128002bc265195b89a3f9c4f389c6717de9351c4
paulo-augustorocha-juniorpartner <pauloaugusto.rochajunior@altran.com>
2021-11-12 12:01:40 +0000

    Parse as bytearray instead of BitStream

--- h26x_extractor/h26x_parser.py
+++ h26x_extractor/h26x_parser.py
@@ -29,17 +29,15 @@ import sys
 import os
 from . import nalutypes
 
-
 class H26xParser:
     """
     H.264 extractor for Annex B streams.
     """
 
-    START_CODE_PREFIX = "0x00000001"
-    START_CODE_PREFIX_SHORT = "0x000001"
     VALID_CALLBACKS = ["sps", "pps", "slice", "aud", "nalu"]
+    startCodePrefixShort =  b'\x00\x00\x01'
 
-    def __init__(self, f, verbose=False, use_bitstream=None):
+    def __init__(self, f, verbose=False):
         """
         Create a new extractor for a .264/h264 file in Annex B format.
 
@@ -47,22 +45,39 @@ class H26xParser:
         use_bitstream: blob to use as bitstream (for testing)
         verbose: whether to print out NAL structure and fields
         """
-        if use_bitstream:
-            # testing the parser in a bitstream
-            self.file = None
-            self.stream = BitStream(use_bitstream)
-        else:
-            fn, ext = os.path.splitext(os.path.basename(f))
-            valid_input_ext = [".264", ".h264"]
-            # TODO: extend for H.265
-            # valid_input_ext = ['.264', 'h264', '.265', '.h265']
-            if ext not in valid_input_ext:
-                raise RuntimeError("Valid input types: " + str(valid_input_ext))
-            bitstream_file = f
-            self.file = bitstream_file
-            self.stream = BitStream(filename=bitstream_file)
+        fn, ext = os.path.splitext(os.path.basename(f))
+        valid_input_ext = [".264", ".h264"]
+        # TODO: extend for H.265
+        # valid_input_ext = ['.264', 'h264', '.265', '.h265']
+        if ext not in valid_input_ext:
+            raise RuntimeError("Valid input types: " + str(valid_input_ext))
+        bitstream_file = f
+        self.file = bitstream_file
+        # Open file as byte stream:
+        with open(bitstream_file, "rb") as f:
+            self.byte_stream = f.read()
+            self.byte_stream = bytearray(self.byte_stream)
+        self.nalu_pos = self._get_nalu_pos()
         self.verbose = verbose
         self.callbacks = {}
+                     
+    def getRSBP(self, start, end):
+        """
+        Get the Rbsp from the NAL unit.
+        """
+        rbsp_enc = self.byte_stream[start:end]
+        rbsp_dec = list()
+        i = 0
+        i_max = len(rbsp_enc)
+        while i < i_max:
+            if (i + 2 < i_max) and (rbsp_enc[i:i+3] == b'\x00\x00\x03'):
+                rbsp_dec.append(rbsp_enc[i])
+                rbsp_dec.append(rbsp_enc[i+1])
+                i += 2
+            else:
+                rbsp_dec.append(rbsp_enc[i])
+            i += 1
+        return rbsp_dec
 
     def set_callback(self, name, fun):
         """
@@ -105,66 +120,45 @@ class H26xParser:
         else:
             self.callbacks[name](*args)
 
-    def _get_nalu_positions(self):
+    def _get_nalu_pos(self):
         """
-        Saves all the NALU positions as bit positions in self.nal_unit_positions
+        Find the start codes in the input file.
         """
-        self.nal_unit_positions = list(
-            self.stream.findall(self.START_CODE_PREFIX, bytealigned=True)
-        )
-        self.short_nal_unit_positions = list(
-            self.stream.findall(self.START_CODE_PREFIX_SHORT, bytealigned=True)
-        )
-
-        if not self.nal_unit_positions and not self.short_nal_unit_positions:
-            print("No NALUs found in stream")
-            sys.exit(1)
-
-        if not self.nal_unit_positions:
-            self.nal_unit_positions = self.short_nal_unit_positions
-        else:
-            # if there were extraneous 3-byte NAL unit start codes, use them too
-            extra_nal_unit_pos = set(
-                [max(s - 8, 0) for s in self.short_nal_unit_positions]
-            ) - set(self.nal_unit_positions)
-            if len(extra_nal_unit_pos) and self.nal_unit_positions:
-                if self.verbose:
-                    print("Warning: 3-byte extra NAL unit start code found")
-                self.nal_unit_positions.extend([s + 8 for s in extra_nal_unit_pos])
-                self.nal_unit_positions = sorted(self.nal_unit_positions)
-
-        self.end_of_stream = len(self.stream)
-        self.nal_unit_positions.append(self.end_of_stream)
-        return self.nal_unit_positions
-
-    def _decode_nalu(self, nalu_bytes):
-        """
-        Returns nal_unit_type and RBSP payload from a NALU stream
-        """
-        if "0x" + nalu_bytes[0 : 4 * 8].hex == self.START_CODE_PREFIX:
-            start_code = nalu_bytes.read("bytes:4")
-        else:
-            start_code = nalu_bytes.read("bytes:3")
-        forbidden_zero_bit = nalu_bytes.read(1)
-        nal_ref_idc = nalu_bytes.read("uint:2")
-        nal_unit_type = nalu_bytes.read("uint:5")
-        nal_unit_payload = nalu_bytes[nalu_bytes.pos :]
-
-        rbsp_payload = BitStream()
-        for i in range(int(len(nal_unit_payload) / 8)):
-            if (
-                len(nal_unit_payload) - nal_unit_payload.pos >= 24
-                and nal_unit_payload.peek("bits:24") == "0x000003"
-            ):
-                rbsp_payload.append(nal_unit_payload.read("bits:8"))
-                rbsp_payload.append(nal_unit_payload.read("bits:8"))
-                nal_unit_payload.read("bits:8")
+        size = self.byte_stream.__len__()
+        nals = []
+        retnals = []
+        
+        pos = 0
+        while pos < size:
+            is4bytes = False
+            retpos = self.byte_stream.find(self.startCodePrefixShort, pos)
+            if retpos == -1:
+                break
+            if self.byte_stream[retpos-1] == 0:
+                retpos -= 1
+                is4bytes = True
+            if is4bytes:
+                pos = retpos + 4
             else:
-                if nal_unit_payload.pos == len(nal_unit_payload):
-                    continue
-                rbsp_payload.append(nal_unit_payload.read("bits:8"))
-
-        return nal_unit_type, rbsp_payload
+                pos = retpos + 3
+            val = hex(self.byte_stream[pos])
+            val = "{0:#0{1}x}".format(self.byte_stream[pos],4)
+            bitField = BitStream (val)
+            fb = bitField.read(1).uint
+            nri = bitField.read(2).uint
+            type = bitField.read(5).uint
+            nals.append((pos, is4bytes, fb, nri, type))
+        for i in range(0, len(nals)-1):
+            start = nals[i][0]
+            if (nals[i+1][1]):
+                end = nals[i+1][0] -5
+            else:
+                end = nals[i+1][0] -4
+            retnals.append((start, end, nals[i][1], nals[i][2], nals[i][3], nals[i][4]))
+        start = nals[-1][0]
+        end = self.byte_stream.__len__() - 1
+        retnals.append((start, end, nals[-1][1], nals[-1][2], nals[-1][3], nals[-1][4]))
+        return retnals
 
     def parse(self):
         """
@@ -172,18 +166,15 @@ class H26xParser:
         Call the respective callbacks for each NALU type found.
         """
 
-        self._get_nalu_positions()
-        nalu_sps = None
-        nalu_pps = None
-        for current_nalu_pos, next_nalu_pos in zip(
-            self.nal_unit_positions, islice(self.nal_unit_positions, 1, None)
-        ):
-            current_nalu_bytepos = int(current_nalu_pos / 8)
-            next_nalu_bytepos = int(next_nalu_pos / 8)
-            nalu_bytes = self.stream[current_nalu_pos:next_nalu_pos]
+        self._get_nalu_pos()
 
-            self.__call("nalu", nalu_bytes)
-
+        for idx, (start, end, is4bytes, fb, nri, type) in enumerate(self.nalu_pos):
+            #print("NAL#%d: %d, %d, %d, %d, %d" % (idx, start, end, fb, nri, type))
+            if (is4bytes):
+                _start = start - 4
+            else:
+                _start = start - 3
+            
             if self.verbose:
                 print("")
                 print(
@@ -192,53 +183,57 @@ class H26xParser:
                 print("")
                 print(
                     "NALU bytepos:\t["
-                    + str(current_nalu_bytepos)
+                    + str(_start)
                     + ", "
-                    + str(next_nalu_bytepos - 1)
+                    + str(end)
                     + "]"
                 )
-                print("NALU offset:\t" + str(current_nalu_bytepos) + " Bytes")
+                print("NALU offset:\t" + str(_start) + " Bytes")
                 print(
                     "NALU length:\t"
-                    + str(next_nalu_bytepos - current_nalu_bytepos)
+                    + str(end - _start +1 )
                     + " Bytes (including start code)"
                 )
-
-            current_nalu_stream_segment = BitStream(
-                self.stream[current_nalu_pos:next_nalu_pos]
-            )
-
-            nal_unit_type, rbsp_payload = self._decode_nalu(current_nalu_stream_segment)
+            rbsp_payload = self.getRSBP(start+1, end+1)
 
             if self.verbose:
                 print(
                     "NALU type:\t"
-                    + str(nal_unit_type)
+                    + str(type)
                     + " ("
-                    + nalutypes.get_description(nal_unit_type)
+                    + nalutypes.get_description(type)
                     + ")"
                 )
-                print("NALU bytes:\t" + str(nalu_bytes))
-                print("NALU RBSP:\t" + str(rbsp_payload))
+                # print("NALU bytes:\t" + '0x' +(self.byte_stream[_start:end+1].hex()))
+                # print("NALU RBSP:\t" + '0x' + bytearray(rbsp_payload).hex())
+                # print("")
+                substr = (self.byte_stream[_start:end+1].hex())
+                if len(substr) > 250:
+                    substr = substr[:250] + "..."
+                print("NALU bytes:\t" + '0x' + substr)
+                substr = bytearray(rbsp_payload).hex()
+                if len(substr) > 250:
+                    substr = substr[:250] + "..."
+                print("NALU RBSP:\t" + '0x' + substr)
                 print("")
 
-            if nal_unit_type == nalutypes.NAL_UNIT_TYPE_SPS:
-                nalu_sps = nalutypes.SPS(rbsp_payload, self.verbose)
-                self.__call("sps", rbsp_payload)
-            elif nal_unit_type == nalutypes.NAL_UNIT_TYPE_PPS:
-                nalu_pps = nalutypes.PPS(rbsp_payload, self.verbose)
-                self.__call("pps", rbsp_payload)
-            elif nal_unit_type == nalutypes.NAL_UNIT_TYPE_AUD:
-                aud = nalutypes.AUD(rbsp_payload, self.verbose)
-                self.__call("aud", rbsp_payload)
-            elif nal_unit_type == nalutypes.NAL_UNIT_TYPE_CODED_SLICE_NON_IDR:
+            rbsp_payload_bs = BitStream(bytearray(rbsp_payload))
+            if type == nalutypes.NAL_UNIT_TYPE_SPS:
+                nalu_sps = nalutypes.SPS(rbsp_payload_bs, self.verbose)
+                self.__call("sps", rbsp_payload_bs)
+            elif type == nalutypes.NAL_UNIT_TYPE_PPS:
+                nalu_pps = nalutypes.PPS(rbsp_payload_bs, self.verbose)
+                self.__call("pps", rbsp_payload_bs)
+            elif type == nalutypes.NAL_UNIT_TYPE_AUD:
+                aud = nalutypes.AUD(rbsp_payload_bs, self.verbose)
+                self.__call("aud", rbsp_payload_bs)
+            elif type == nalutypes.NAL_UNIT_TYPE_CODED_SLICE_NON_IDR:
                 nalu_slice = nalutypes.CodedSliceNonIDR(
-                    rbsp_payload, nalu_sps, nalu_pps, self.verbose
+                    rbsp_payload_bs, nalu_sps, nalu_pps, self.verbose
                 )
-                self.__call("slice", rbsp_payload)
-            elif nal_unit_type == nalutypes.NAL_UNIT_TYPE_CODED_SLICE_IDR:
+                self.__call("slice", rbsp_payload_bs)
+            elif type == nalutypes.NAL_UNIT_TYPE_CODED_SLICE_IDR:
                 nalu_slice = nalutypes.CodedSliceIDR(
-                    rbsp_payload, nalu_sps, nalu_pps, self.verbose
+                    rbsp_payload_bs, nalu_sps, nalu_pps, self.verbose
                 )
-                self.__call("slice", rbsp_payload)
-
+                self.__call("slice", rbsp_payload_bs)
